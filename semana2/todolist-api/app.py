50984337f99a6ffbbeadcf5eb971ae7c0312ed25
linder3hs <linderhassinger00@gmail.com>
2023-11-17 23:00:50 -0500

    add db

--- semana2/todolist-api/app.py
+++ semana2/todolist-api/app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from app.routes.tasks import task_route
+from app.db import db
+from app.config import Config
 
 # instancia de Flaks
 app = Flask(__name__)
 
+app.config.from_object(Config)
+
 app.register_blueprint(task_route)
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == "__main__":
---
+++ semana2/todolist-api/app/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = "customsecretkey"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:root@127.0.0.1:3306/codigo_15_flask"
+    SQLALCHEMY_TRACKS_MODIFICATIONS = False
