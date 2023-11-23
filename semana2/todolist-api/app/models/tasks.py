5f084c7172127cee842d176ccd2171903fccd06b
linder3hs <linderhassinger00@gmail.com>
2023-11-22 19:51:29 -0500

    ➕ feat: add class tasks

---
+++ semana2/todolist-api/app/models/tasks.py
@@ -0,0 +1,19 @@
+from app.db import db
+
+
+class Task(db.Model):
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    title = db.Column(db.String(255))
+    priority = db.Column(db.String(255))
+    category = db.Column(db.String(255))
+    status = db.Column(db.String(50))
+    due_date = db.Column(db.Date())
+    is_done = db.Column(db.Boolean)
+
+    def __init__(self, title, priority, category, status, due_date, is_done):
+        self.title = title
+        self.priority = priority
+        self.category = category
+        self.status = status
+        self.due_date = due_date
+        self.is_done = is_done
