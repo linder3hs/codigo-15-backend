765f0d65cebf594498e3d4a41676e3f06cee0932
linder3hs <linderhassinger00@gmail.com>
2023-11-22 22:39:13 -0500

    ➕ feat: add post

--- semana2/todolist-api/app/routes/tasks.py
+++ semana2/todolist-api/app/routes/tasks.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, request
-from datetime import date
-from utils import search_task, response_success, response_error
+from utils import response_success, response_error
 from app.models.tasks import Task
+from app.db import db
 
 task_route = Blueprint('task_route', __name__)
 
 
 @task_route.route("/tasks")
 def get_tasks():
-    tasks = Task.query.all()
+    tasks = Task.query.all()  # SELECT * FROM tasks
     # comprehesion (for en una linea)
     serialized_tasks = [task.to_json() for task in tasks]
     """
@@ -19,23 +19,31 @@ def get_tasks():
 
     return response_success(serialized_tasks)
 
-#
-# @task_route.route("/tasks/<int:task_id>")
-# def get_task(task_id):
-#     result = search_task(tasks, task_id)
-#     if result is None:
-#         return response_error("Task not found")
-#
-#     return response_success(result)
-#
-#
-# @task_route.route("/tasks", methods=["POST"])
-# def add_task():
-#     task = request.json
-#     task["id"] = len(tasks) + 1
-#     task["created_at"] = date.today()
-#     tasks.append(task)
-#     return response_success("Tarea creada correctamente", 201)
+
+@task_route.route("/tasks/<int:task_id>")
+def get_task(task_id):
+    result = Task.query.get(task_id)  # SELECT * FROM tasks WHERE id=task_id3
+    """
+    Esto solo obiene 1 element de tipo Task
+    recordatorio para poder leer este objeto tenemos que transformarlo
+    a un json y para ello hemos creado al funcion to_json()
+    """
+    if result is None:
+        return response_error("Task not found")
+
+    return response_success(result.to_json())
+
+
+@task_route.route("/tasks", methods=["POST"])
+def add_task():
+    try:
+        task = Task(**request.get_json())
+        db.session.add(task)
+        db.session.commit()
+
+        return response_success("Tarea creada correctamente", 201)
+    except Exception as e:
+        return response_error(str(e))
 #
 #
 # @task_route.route("/tasks/<int:task_id>", methods=["PUT"])
