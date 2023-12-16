45178f2f5119665ac5806009e2a4d7cd7d8c6275
linder3hs <linderhassinger00@gmail.com>
2023-12-15 20:11:44 -0500

    ➕ feat: user actor

--- semana6/netflix-api/actor/models.py
+++ semana6/netflix-api/actor/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Actor(models.Model):
@@ -11,6 +12,7 @@ class Actor(models.Model):
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"{self.name} {self.last_name}"
--- semana6/netflix-api/user/serializers.py
+++ semana6/netflix-api/user/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
 
 class UserSerializer(ModelSerializer):
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
     class Meta:
         model = User
         fields = "__all__"
