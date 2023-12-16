727b4d4fde0beb244a08dc641698a2ebd1adf63c
linder3hs <linderhassinger00@gmail.com>
2023-12-15 20:49:07 -0500

    🐛 bugfix: add user

--- semana6/netflix-api/user/urls.py
+++ semana6/netflix-api/user/urls.py
@@ -1,11 +1,14 @@
 from .views import UserViewSet
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 router = DefaultRouter()
 router.register(r'user', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('token', TokenObtainPairView.as_view(), name="Get JWT"),
+    path('token/refresh', TokenRefreshView.as_view(), name="Refresh JWT")
 ]
