ea3f7d26c5a3ec18af1c7e8db17c269d01ebbb63
linder3hs <linderhassinger00@gmail.com>
2024-01-04 22:26:22 -0500

    ➕ feat: add components folder

---
+++ semana8/api-store/src/components/user/controller.js
@@ -0,0 +1,76 @@
+import { PrismaClient } from "@prisma/client";
+import { responseSuccess, responseError } from "../../network/responses.js";
+
+const prisma = new PrismaClient();
+
+export async function list(req, res) {
+  try {
+    const users = await prisma.user.findMany();
+    return responseSuccess({ res, data: users });
+  } catch (error) {
+    return responseError({ res, data: error.message });
+  }
+}
+
+export async function getById(req, res) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+
+    if (!user) {
+      return responseError({ res, data: "User not found" });
+    }
+
+    return responseSuccess({ res, data: user });
+  } catch (error) {
+    return responseError({ res, data: error.message });
+  }
+}
+
+export async function store(req, res) {
+  try {
+    await prisma.user.create({
+      data: req.body,
+    });
+
+    return responseSuccess({ res, data: "User created", status: 201 });
+  } catch (error) {
+    return responseError({ res, data: error.message });
+  }
+}
+
+export async function update(req, res) {
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: Number(req.params.id),
+      },
+      data: req.body,
+    });
+
+    if (!user) {
+      return responseError({ res, data: "User not found" });
+    }
+
+    return responseSuccess({ res, data: "User updated" });
+  } catch (error) {
+    return responseError({ res, data: error.message });
+  }
+}
+
+export async function destroy(req, res) {
+  try {
+    await prisma.user.delete({
+      where: {
+        id: Number(req.params.id),
+      },
+    });
+
+    return responseSuccess({ res, data: "User deleted" });
+  } catch (error) {
+    return responseError({ res, data: error.message });
+  }
+}
---
+++ semana8/api-store/src/components/user/network.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import * as Controller from "./controller.js";
+
+const userRouter = Router();
+
+// userRouter.get("/", Controller.list);
+userRouter.route("/").get(Controller.list);
+userRouter.route("/:id").get(Controller.getById);
+userRouter.route("/").post(Controller.store);
+userRouter.route("/:id").put(Controller.update);
+userRouter.route("/:id").delete(Controller.destroy);
+
+export default userRouter;
