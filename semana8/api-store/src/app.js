a9091480647c14cd92d6cdc6981932abd0c26299
linder3hs <linderhassinger00@gmail.com>
2024-01-05 23:01:19 -0500

    ➕ feat: add cors

--- semana8/api-store/src/app.js
+++ semana8/api-store/src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { apiVersion } from "./config";
 import { userRouter, productRouter } from "./components";
+import cors from "cors";
 
 export const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.use(`${apiVersion}/users`, userRouter);
