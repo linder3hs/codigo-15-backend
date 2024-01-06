985e83e6de83f190dec5dff4fac1e6decfed823d
linder3hs <linderhassinger00@gmail.com>
2024-01-05 20:20:18 -0500

    ➕ feat: add config

--- semana8/api-store/src/app.js
+++ semana8/api-store/src/app.js
@@ -1,7 +1,8 @@
 import express from "express";
+import { apiVersion } from "./config";
 import { userRouter } from "./components";
 
 export const app = express();
 app.use(express.json());
 
-app.use("/api/v1/users", userRouter);
+app.use(`${apiVersion}/users`, userRouter);
---
+++ semana8/api-store/src/config/index.js
@@ -0,0 +1,1 @@
+export const apiVersion = "/api/v1";
